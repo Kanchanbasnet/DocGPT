31a3ed27a778684f976c452323053535050fc6a4
kanchan Basnet <kanchan.basnet@amniltech.com>
2025-09-03 22:55:18 +0545

    feat: chunk docs

---
+++ backend/src/services/chunkDocs.ts
@@ -0,0 +1,42 @@
+
+import { DirectoryLoader } from 'langchain/document_loaders/fs/directory';
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { DocxLoader } from '@langchain/community/document_loaders/fs/docx';
+import { PPTXLoader } from '@langchain/community/document_loaders/fs/pptx';
+import { TextLoader } from 'langchain/document_loaders/fs/text';
+import {
+    RecursiveCharacterTextSplitter,
+} from 'langchain/text_splitter'
+
+
+
+
+
+export const chunkDocs = async () => {
+    let directoryPath = 'chunkUploads';
+    let docs: any = [];
+
+
+
+    try {
+        const directoryLoader = new DirectoryLoader(directoryPath, {
+            '.pdf': (filePath) => new PDFLoader(filePath),
+            '.docx': (filePath) => new DocxLoader(filePath),
+            '.pptx': (filePath) => new PPTXLoader(filePath),
+            '.txt': (filePath) => new TextLoader(filePath)
+        })
+        const rawDocs = await directoryLoader.load();
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 1000,
+            chunkOverlap: 200
+        })
+        docs = docs.concat(await textSplitter.splitDocuments(rawDocs));
+        return docs;
+
+    }
+    catch (error) {
+        console.log("An error has been occured:::", error);
+        
+
+    }
+}
