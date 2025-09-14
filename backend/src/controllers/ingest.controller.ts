db04cd6fab1d10e7c82b401adc828fba90a8aa38
kanchan Basnet <kanchan.basnet@amniltech.com>
2025-09-14 13:57:27 +0545

    feat: ingestion

--- backend/src/controllers/ingest.controller.ts
+++ backend/src/controllers/ingest.controller.ts
@@ -1,15 +1,65 @@
 import { Request, Response } from "express";
+import User from "../models/users/user.model";
+import DataIngest from "../models/ingest/ingest.model";
 
 
+export const getAllIngest = async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
 
+        if (!userId) {
+            return res.status(400).json({ error: "Please provide the userId" });
+        }
 
-export const createIngest = async(req: Request, res: Response)=>{
-    try{
-        const {userId} = req.body;
-        const {files} = req.body.files;
+        const userExist = await User.findById(userId);
+        if (!userExist) {
+            return res.status(404).json({ error: "User not found." });
+        }
 
+        const ingest = await DataIngest.find({ userId });
 
-    }catch(error){
+        return res.status(200).json({
+            message: "Ingest retrieved successfully.",
+            data: ingest,
+        });
+    } catch (error: any) {
+        console.error("Could not get all ingest", error);
+        return res.status(500).json({
+            error: "An error occurred while retrieving ingests.",
+            details: error.message || error,
+        });
+    }
+};
+
+export const getIngestById = async (req: Request, res: Response) => {
+    try {
+        const { ingestId } = req.params;
+        if (!ingestId) {
+            return res.status(400).json({ error: "Please provide the ingestId" })
+        }
+        const ingestExist = await DataIngest.findById(ingestId);
+        if (!ingestExist) {
+            return res.status(404).json({ error: "DataIngest not Found." });
+        }
+        return res.status(200).json({
+            message: "Ingest retrieved successfully.",
+            data: ingestExist
+        })
+
+    } catch (error: any) {
+        console.error("Could not get all ingest", error);
+        return res.status(500).json({
+            error: "An error occurred while retrieving ingests.",
+            details: error.message || error,
+        });
+
+    }
+}
+
+export const createIngest = (req: Request, res: Response) => {
+    try {
+
+    } catch (error) {
 
     }
 
